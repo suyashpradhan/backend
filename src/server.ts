d512215a16a5db0cf61c7b316235cfc397855ad6
Suyash Pradhan <pradhansuyash39@gmail.com>
2023-09-01 23:03:21 +0530

    feat: adds routes

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,11 @@ app.get('/', (_, res) => {
 	res.json({ message: 'Hello World!' });
 });
 
+app.get('/info', (_, res) => {
+	res.status(200);
+	res.json({ message: 'Info Route' });
+});
+
 app.listen(3000, () => {
 	console.log('Server on port 3000');
 });
